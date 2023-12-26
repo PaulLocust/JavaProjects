72dce412ba81f54e73d73979c36028db47b2aa48
Pavel Sarancha <68758084+PaulLocust@users.noreply.github.com>
2023-12-26 17:33:00 +0300

    Add files via upload

---
+++ Snake/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    int speed;
+    int weight;
+    String colour;
+    Test(int speed, int weight, String colour) {
+        this.speed = speed;
+        this.weight = weight;
+        this.colour = colour;
+        System.out.println("speed is: " + speed);
+        System.out.println("weight is: " + weight);
+        System.out.println("colour is: " + colour);
+    }
+}
