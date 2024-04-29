a9155d1215b0879177f298d7056d68ea4bdb8387
Pavel Sarancha <68758084+PaulLocust@users.noreply.github.com>
2024-04-29 20:41:40 +0300

    Delete common directory

--- common/src/exceptions/OpeningServerSocketException.java
+++
@@ -1,4 +0,0 @@
-package exceptions;
-
-public class OpeningServerSocketException extends Exception{
-}
--- common/src/exceptions/ScriptRecursionException.java
+++
@@ -1,4 +0,0 @@
-package exceptions;
-
-public class ScriptRecursionException extends Exception{
-}
